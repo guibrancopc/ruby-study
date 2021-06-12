5cf510f3819bd9c99b5a5e0272ae9d7db27f84b2
Guilherme Carvalho <guibrancopc@gmail.com>
2021-06-12 19:55:03 -0300

    feat: add article resource with read phase done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # root to: "articles#index"
+  get "/" => redirect('/articles')
+
+  resources :articles # does de same of both lines below
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
 end
