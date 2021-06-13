790a215f40d9315b454a1c784b4c42afec1b3f91
Guilherme Carvalho <guibrancopc@gmail.com>
2021-06-13 13:35:44 -0300

    chore: repository renamed to ruby-study

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "zombies_twitter_production"
+  # config.active_job.queue_name_prefix = "ruby_study_production"
 
   config.action_mailer.perform_caching = false
 
